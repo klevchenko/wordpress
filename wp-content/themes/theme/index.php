b2435a50756815ce9fae1da98d5d7e896e776258
klevchenko <kostialevshenko1>
2020-07-23 22:04:08 +0300

    init

---
+++ wp-content/themes/theme/index.php
@@ -0,0 +1,20 @@
+<?php
+
+get_header();
+?>
+
+<?php
+if ( have_posts() ) {
+
+    // Load posts loop.
+    while ( have_posts() ) {
+        the_post();
+        the_title();
+    }
+
+} else {
+
+}
+?>
+<?php
+get_footer();
